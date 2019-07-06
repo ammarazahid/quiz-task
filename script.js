cba50b485d9b4db32e821ed7cef06cbb370fa412
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-06 12:05:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 let score = 0;
-let quaideazam = Quaid-e-Azam; 
-let fourp =  Four;
-let jinnah = Muhammad Ali Jinnah;
-let karachi = Karachi;
-let mango = Mango;
+// let quaideazam = Quaid-e-Azam; 
+// let fourp =  Four;
+// let jinnah = Muhammad Ali Jinnah;
+// let karachi = Karachi;
+// let mango = Mango;
 
 
 function quidScore() {
+   let correct = document.querySelector(".quideazam").checked;
+   if (correct == true){
     score += 1;
+   }
 }
 
 function fourScore() {
-    score += 1;
+    let correct = document.querySelector(".four").checked;
+    if (correct == true){
+     score += 1;
+    }
 }
 
 function karachiScore() {
-    score += 1;
+    let correct = document.querySelector(".karachi").checked;
+    if (correct == true){
+     score += 1;
+    }
 }
 
 function mangoScore() {
-    score += 1;
+    let correct = document.querySelector(".mango").checked;
+    if (correct == true){
+     score += 1;
+    }
 }
 
 function jinnahScore() {
-    score += 1;
+    let correct = document.querySelector(".jinnah").checked;
+    if (correct == true){
+     score += 1;
+    }
 }
 
 function check() {
-    if (quaideazam == true &&
-    fourp == true &&
-    jinnah == true &&
-    karachi == true &&
-    mango == true) {
-        alert("congratulations!! You did very well. Your score is" + score + "out of 5");
+    if (score == 5) {
+        alert("congratulations!! You did very well. Your score is"  + score +  "out of 5");
     } else {
-        alert("your Score is" + score + "out of 5");
+        alert("your Score is"  + score +   "out of 5");
     }
 }
