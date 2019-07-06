04e1ce3ec1c3ad3e3a41cf7e06d9f4c66f780365
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-06 21:20:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,29 +14,29 @@ function quidScore() {
 }
 
 function fourScore() {
-    let correct = document.querySelector(".four").checked;
-    if (correct == true){
+    let correct1 = document.querySelector(".four").checked;
+    if (correct1 == true){
      score += 1;
     }
 }
 
 function karachiScore() {
-    let correct = document.querySelector(".karachi").checked;
-    if (correct == true){
+    let correct2 = document.querySelector(".karachi").checked;
+    if (correct2 == true){
      score += 1;
     }
 }
 
 function mangoScore() {
-    let correct = document.querySelector(".mango").checked;
-    if (correct == true){
+    let correct3 = document.querySelector(".mango").checked;
+    if (correct3 == true){
      score += 1;
     }
 }
 
 function jinnahScore() {
-    let correct = document.querySelector(".jinnah").checked;
-    if (correct == true){
+    let correct4 = document.querySelector(".jinnah").checked;
+    if (correct4 == true){
      score += 1;
     }
 }
@@ -44,7 +44,9 @@ function jinnahScore() {
 function check() {
     if (score == 5) {
         alert("congratulations!! You did very well. Your score is"  + score +  "out of 5");
-    } else {
+    } else if (score < 5){
         alert("your Score is"  + score +   "out of 5");
+    } else{
+        alert("some thing went wrong!");
     }
 }
